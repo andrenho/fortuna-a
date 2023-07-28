27e95048c4c1245830b3edb5af823fa1bacf2bd4
André Wagner <andre.nho@gmail.com>
2023-07-28 09:06:31 -0500

    .

--- debugger/debugger.js
+++ debugger/debugger.js
@@ -1,10 +1,28 @@
-/*
- * TABS
- */
+//
+// GLOBALS
+// 
 
-let selectedTab = "code";
+let tabSelected = "code";
+let memoryPage = 0x20;
 
-function selectTab(tab) {
+//
+// EVENTS
+//
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == "1")
+        tabSelect("code");
+    else if (e.key == "2")
+        tabSelect("memory");
+    else if (e.key == "3")
+        tabSelect("selftest");
+});
+
+//
+// TABS
+//
+
+function tabSelect(tab) {
     const el = e(`tab-${tab}`);   
     if (el) {
         for (const elem of ["code", "memory", "selftest"]) {
@@ -15,10 +33,44 @@ function selectTab(tab) {
         el.classList.add("tab-selected");
         e(tab).style.display = "flex";
     }
+
+    tabSelected = tab;
+
+    if (tab == "memory")
+        memoryRequestUpdate();
 }
 
-/*
- * UTILS
- */
+//
+// MEMORY
+//
+
+function memoryChangePage(offset) {
+    memoryPage += offset;
+    if (memoryPage > 0xff)
+        memoryPage = 0x0;
+    else if (memoryPage < 0x00)
+        memoryPage = 0xff;
+
+    memoryRequestUpdate();
+}
+
+function memoryRequestUpdate()
+{
+}
+
+//
+// API
+//
+
+const apiUrl = window.location.href;
+
+function apiMemoryRead(page)
+{
+
+}
+
+//
+// UTILS
+//
 
 function e(id) { return document.getElementById(id); }
