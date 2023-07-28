7a0b7f5824c5c9fa71bacdc5780f4b5270dd9fe2
Andre Wagner <WagnerAndre@JohnDeere.com>
2023-07-28 11:57:25 -0500

    .

--- debugger/debugger.js
+++ debugger/debugger.js
@@ -44,33 +44,92 @@ function tabSelect(tab) {
 // MEMORY
 //
 
-function memoryChangePage(offset) {
+async function memoryChangePage(offset) {
     memoryPage += offset;
     if (memoryPage > 0xff)
         memoryPage = 0x0;
     else if (memoryPage < 0x00)
         memoryPage = 0xff;
 
+    e("memory-page").innerHTML = `0x${hex(memoryPage)}`;
+
     memoryRequestUpdate();
 }
 
-function memoryRequestUpdate()
-{
+async function memoryRequestUpdate() {
+    e("memory-holder").style.visibility = "hidden";
+    
+    const array = await apiMemoryRead(memoryPage);
+
+    const tbody = e("memory-body");
+    tbody.innerHTML = '';
+    
+    for (let i = 0; i < 16; ++i) {
+        const tr = document.createElement("tr");
+
+        const addr = document.createElement("td");
+        addr.classList.add("memory-address");
+        addr.innerHTML = hex(memoryPage << 8 | (i << 4), 4);
+        tr.appendChild(addr);
+
+        for (let j = 0; j < 16; ++j) {
+            const data = document.createElement("td");
+            data.classList.add("memory-data");
+            if (j == 7)
+                data.classList.add("memory-data-7");
+            data.innerHTML = hex(array[(i * 16) + j]);
+            tr.appendChild(data);
+        }
+
+        const str = [];
+        for (let j = 0; j < 16; ++j) {
+            const c = array[(i * 16) + j];
+            if (c < 32 || c >= 127)
+                str.push('.');
+            else
+                str.push(String.fromCharCode(c));
+        }
+        const text = document.createElement("td");
+        text.classList.add("memory-text");
+        text.innerHTML = str.join("");
+        tr.appendChild(text);
+
+        tbody.appendChild(tr);
+    }
+
+    e("memory-holder").style.visibility = "visible";
 }
 
 //
 // API
 //
 
-const apiUrl = window.location.href;
+const apiUrl = window.location.href.replace(/\/$/, "");
 
-function apiMemoryRead(page)
-{
+async function callApi(path, options) {
+    try {
+        e("spinner").style.visibility = "visible";
+        const response = await fetch(apiUrl + path, options);
+        if (!response.ok)
+            throw new Error(await response.text());
+        
+        return await response.json();
+    } catch (ex) {
+        e("error").style.display = "block";
+        e("error").innerHTML = ex.message;
+        throw ex;
+    } finally {
+        e("spinner").style.visibility = "hidden";
+    }
+}
 
+async function apiMemoryRead(page) {
+    return callApi(`/memory/${page}`);
 }
 
 //
 // UTILS
 //
 
-function e(id) { return document.getElementById(id); }
+const e = (id) => document.getElementById(id);
+const hex = (num, digits) => num.toString(16).toUpperCase().padStart(digits || 2, '0');
--- debugger/debugger.py
+++ debugger/debugger.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 
+import json
 import http.server
 import socketserver
 
+class Serial:
+
+    def memory_page(self, page):
+        return 256 * [0x65]
+
 class Server(http.server.SimpleHTTPRequestHandler):
+
+    def send_object(self, obj):
+        self.send_response(200, 'OK')
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(obj), 'utf-8'))
 
     def do_GET(self):
         path = self.path.split('?')[0]
+        resource = path[1:].split('/')
         if path == '/' or path.endswith('.html') or path.endswith('.css') or path.endswith('.js'):
             super().do_GET()
+        elif resource[0] == 'memory':
+            page = int(resource[1])
+            self.send_object(serial.memory_page(page))
         else:
             self.send_response(404, 'Not found')
             self.end_headers()
             self.wfile.write(b'404 - Not found.\n')
 
+
+serial = Serial()
 
 socketserver.TCPServer.allow_reuse_address = True
 httpd = socketserver.TCPServer(('127.0.0.1', 8000), Server)
