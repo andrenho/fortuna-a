224fffce6bc5e80e68dca4da9d5f5eb5b7bdd44d
André Wagner <andre.nho@gmail.com>
2023-09-20 21:03:18 -0300

    .

--- firmware/storage/main.c
+++ firmware/storage/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 
 #include "uart.h"
 
+#if 0
 static void reset_flip_flop(void)
 {
     PORTD |= _BV(PD2);
@@ -24,25 +26,44 @@ inline static uint8_t read_data(void)
     return pinb;
 }
 
+static void init_spi(void)
+{
+    USICR = _BV(USIWM0) | _BV(USICS1);  // 3-wire mode, clock from timer overflow
+    USISR = _BV(USIOIF);  // timer overflow interrupt
+
+    sei();
+}
+
 static void send_via_spi(uint8_t data)
 {
-    // TODO
+    PORTD &= ~_BV(PORTD4);
+
+    USIDR = data;
+    USISR = _BV(USIOIF);
+
+    // wait for a complete transfer
+    while (!(USISR & _BV(USIOIF))) {
+        USICR |= _BV(USITC);   // toggle clock
+    }
+
+    PORTD |= _BV(PORTD4);
 }
 
 int main(void)
 {
     uart_init();
+    init_spi();
     puts_P(PSTR("\e[1;1H\e[2JHello!"));
 
     // setup ports
     DDRD |= _BV(DDD2) | _BV(DDD4);  // R, SD_CS = output
+    PORTD |= _BV(PORTD4);
 
-    // setup SPI (TODO)
-    
     // initial setup
     reset_flip_flop();
 
     // main loop
+    /*
     for (;;) {
         while (PIND & _BV(PD3));  // wait until Y2W = 0
 
@@ -53,8 +74,48 @@ int main(void)
 
         reset_flip_flop();
     }
+    */
+
+    for (;;) {
+        send_via_spi(0xaf);
+        _delay_ms(1);
+    }
 
     for (;;);
 
     return 0;
+}
+#endif
+
+int main()
+{
+    DDRB |= _BV(DDB7) | _BV(DDB6);  // SCLK, DO = output
+    DDRD |= _BV(DDD2) | _BV(DDD4);  // R, SD_CS = output
+    PORTD |= _BV(PORTD4);
+
+    for (;;) {
+        PORTD &= ~_BV(PORTD4);
+
+        USIDR = 0xab;
+
+        // Configure SPI settings for MSB
+        uint8_t usicr_msb = (1 << USIWM0) | (0 << USICS0) | (1 << USITC);
+        
+        // Configure SPI settings for LSB
+        uint8_t usicr_lsb = (1 << USIWM0) | (0 << USICS0) | (1 << USITC) | (1 << USICLK);
+
+        // SPI transfer loop for 8 bits
+        for (int i = 0; i < 16; i++) {
+            if (i % 2 == 0) {
+                USICR = usicr_msb;
+            } else {
+                USICR = usicr_lsb;
+            }
+        }
+
+        PORTD |= _BV(PORTD4);
+
+        _delay_ms(1);
+    }
+
 }
