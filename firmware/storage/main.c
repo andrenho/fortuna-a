cf787d350af819ba3f6e2e39bface878afc06b12
André Wagner <andre.nho@gmail.com>
2024-02-15 19:58:30 -0300

    .

--- firmware/storage/main.c
+++ firmware/storage/main.c
@@ -2,17 +2,22 @@
 #include <stdint.h>
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 #include "uart.h"
 
 int main()
 {
-    DDRC |= _BV(PC4);
+    DDRC |= _BV(PC0) | _BV(PC1);
+    PORTC = 0x0;
 
     uart_init();
-    PORTC |= _BV(PC4);
 
     for (;;) {
         printf("Hello.\n");
+        PORTC |= _BV(PC0);
+        _delay_ms(200);
+        PORTC &= ~_BV(PC0);
+        _delay_ms(200);
     }
 }
